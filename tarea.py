e49b6ec4b562ef8eac0ca96f5593ef23f00dda9c
unknown <fran.millan95@gmail.com>
2023-10-12 17:30:00 -0300

    primer commit

---
+++ tarea.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+listaSuper = []
+
+
+# Obtener todos los todos
+@app.route("/api/listaSuper", methods=["GET"])
+def get_listaSuper():
+    return jsonify(listaSuper)
+
+
+# Agrego un nuevo producto en la lista
+@app.route("/api/listaSuper", methods=["POST"])
+def create_todo():
+    prod = request.json
+    listaSuper.append(prod)
+    print(prod)
+    print(listaSuper)
+    return jsonify(message="Producto Agregado")
+
+
+# Obtener un prod por su ID
+@app.route("/api/listaSuper/<int:prod_id>", methods=["GET"])
+def get_prodLista(prod_id):
+    for prod in listaSuper:
+        if prod["id"] == prod_id:
+            print(prod)
+            print(listaSuper)
+            return jsonify(prod), 200
+    return jsonify(message="Producto no encontrado"), 404
+
+
+# Actualizar un todo por su ID
+@app.route("/api/listaSuper/<int:prod_id>", methods=["PUT"])
+def update_lista(prod_id):
+    print(request.json)
+    for prod in listaSuper:
+        if prod["id"] == prod_id:
+            prod["producto"] = request.json["producto"]
+            print(prod)
+            print(listaSuper)
+            return jsonify(message="Producto actualizado exitosamente"), 200
+    return jsonify(message="Producto no encontrado"), 404
+
+
+# Eliminar un todo por su ID
+@app.route("/api/listaSuper/<int:prod_id>", methods=["DELETE"])
+def delete_prod(prod_id):
+    for prod in listaSuper:
+        if prod["id"] == prod_id:
+            print(prod)
+            listaSuper.remove(prod)
+            print(listaSuper)
+            return jsonify(message="Producto eliminado exitosamente"), 200
+    return jsonify(message="Producto no encontrado"), 404
+
+
+if __name__ == "__main__":
+    app.run()
